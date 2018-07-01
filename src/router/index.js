868803b58db0ffbd33fb1b1592295e948a25ca01
vic lotana <viclotana@gmail.com>
2018-07-01 11:36:05 +0100

    adding routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import home from '@/components/home'
+import blog from '@/components/blog'
+import services from '@/components/services'
+import contact from '@/components/contact'
 
 Vue.use(Router)
 
@@ -10,6 +13,21 @@ export default new Router({
       path: '/',
       name: 'home',
       component: home
+    },
+    {
+      path: '/blog',
+      name: 'blog',
+      component: blog
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: contact
+    },
+    {
+      path: '/services',
+      name: 'services',
+      component: services
     }
   ]
 })
