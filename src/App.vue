d513616f8672a86420cd88d6b5dc3f32d7605c11
vic lotana <viclotana@gmail.com>
2018-07-01 10:52:39 +0100

    commiting

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,10 @@
 <template>
   <div id="app">
-    
-
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
   <a class="navbar-brand" href="#">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-
   <div class="collapse navbar-collapse" id="navbarColor01">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
