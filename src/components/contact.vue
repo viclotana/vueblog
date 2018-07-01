21669605e9ae219d5921740e263946f0f65cfc73
vic lotana <viclotana@gmail.com>
2018-07-01 12:03:02 +0100

    rolling

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -1,6 +1,6 @@
 <template>
  <div class="contact">
-  <h1>{{contact}}</h1>
+  <h1>Contact</h1>
  </div>
 </template>
 <script>
--- src/components/services.vue
+++ src/components/services.vue
@@ -1,6 +1,6 @@
 <template>
  <div class="services">
-  <h1>{{services}}</h1>
+  <h1>Services</h1>
  </div>
 </template>
 <script>
