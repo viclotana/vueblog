13d5a16ab014e985ad2ecba2c8f237044d18be69
vic lotana <viclotana@gmail.com>
2018-07-03 20:26:01 +0100

    adding shoes

--- src/components/home.vue
+++ src/components/home.vue
@@ -9,7 +9,34 @@ export default {
   name: 'home',
   data () {
     return {
-      title: 'home'
+      title: 'home',
+      shoes: [
+        {
+          shoeName: "Solar Red",
+          image: require('../assets/yeezy1.jpg'),
+          shoeId: 1
+        },
+        {
+          shoeName: "Black Red",
+          image: require('../assets/yeezy2.jpg'),
+          shoeId: 2
+        },
+        {
+          shoeName: "Frozen Yellow",
+          image: require('../assets/yeezy3.jpg'),
+          shoeId: 3
+        },
+        {
+          shoeName: "Zebra V2",
+          image: require('../assets/yeezy4.jpg'),
+          shoeId: 4
+        },
+        {
+          shoeName: "Beluga Grey",
+          image: require('../assets/yeezy5.jpg'),
+          shoeId: 5
+        }
+      ]
     }
   }
 }
